a3bb8077749524c4162bfa2edcab5172c2fdf4c6
Thanseef <androtsf@gmail.com>
2023-02-03 10:25:45 +0530

    code cleaning done

--- DataPreparing/Movie_Updater.py
+++ DataPreparing/Movie_Updater.py
@@ -5,7 +5,7 @@ import os
 import requests
 from tmdbv3api import TMDb
 from tmdbv3api import Movie
-from S3_Uploader import Trigger_Uploader #S3 Trigger function calling.
+from S3_Uploader import Trigger_Uploader
 from datetime import datetime
 from dotenv import load_dotenv
 load_dotenv()
@@ -19,119 +19,140 @@ tmdb.api_key = API_KEY
 file_path = 'PreparedData/'
 file_name = "UpdatedDataSet.csv"
 list_of_months = {'1': 'January', '2': 'February', '3': 'March',
-                   '4': 'April', '5': 'May', '6': 'June', '7': 'July',
-                   '8': 'August', '9': 'September', '10': 'October',
-                   '11': 'November', '12': 'December'}
+                  '4': 'April', '5': 'May', '6': 'June', '7': 'July',
+                  '8': 'August', '9': 'September', '10': 'October',
+                  '11': 'November', '12': 'December'}
 
 #################################### Generating Dataset ######################################
 
+
 def get_genre(x):
+    """
+        1. Here Searching the Movie Titles what we have got from Wikipedia
+        2. Collecting Genres from TMDB 
+    """
     genres = []
     result = tmdb_movie.search(x)
     if result:
         movie_id = result[0].id
-        response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id,tmdb.api_key))
+        response = requests.get(
+            'https://api.themoviedb.org/3/movie/{}?api_key={}'.format(movie_id, tmdb.api_key))
         data_json = response.json()
         if data_json['genres']:
-            genre_str = " " 
-            for i in range(0,len(data_json['genres'])):
+            genre_str = " "
+            for i in range(0, len(data_json['genres'])):
                 genres.append(data_json['genres'][i]['name'])
             return genre_str.join(genres)
     else:
         np.NaN
-        
+
+
 def get_director(x):
     if " (director)" in x:
-        # this will split director
         return x.split(" (director)")[0]
     elif " (directors)" in x:
-        # this will split directors
         return x.split(" (directors)")[0]
     else:
         return x.split(" (director/screenplay)")[0]
-      
+
+
 def get_actor_one(x):
     return ((x.split("screenplay); ")[-1]).split(", ")[0])
+
 
 def get_actor_two(x):
     if len((x.split("screenplay); ")[-1]).split(", ")) < 2:
         return np.NaN
     else:
         return ((x.split("screenplay); ")[-1]).split(", ")[1])
-      
+
+
 def get_actor_three(x):
     if len((x.split("screenplay); ")[-1]).split(", ")) < 3:
         return np.NaN
     else:
         return ((x.split("screenplay); ")[-1]).split(", ")[2])
-      
+
+
 def s3_updated_dataset():
-    data = pd.read_csv('https://movie-dataset-live.s3.ap-south-1.amazonaws.com/PreparedData/UpdatedDataSet.csv')
+    data = pd.read_csv(
+        'https://movie-dataset-live.s3.ap-south-1.amazonaws.com/PreparedData/UpdatedDataSet.csv')
     return data
 
+
 def preprocess_data(df):
+    """
+        1. Here Creating Feature List that we have used to make our old Dataset.
+        2. Renaming the Dataframe Fields
+        3. Replacing Nan Values with Unknown to avoid Conflict
+        4. Lower casing the Title of the movies
+        5. Combining all the data's
+        6. Dropping values contain Nan
+        7. Taking Previously updated Dataset From S3 bucket
+        8. Merging Old and New Dataset
+        9. Dropping Duplicate Movie Details
+        10. Saving Dataset
+        11. Uploading to S3 bucket (which will replace the old one with new one)
+    """
     try:
-        # Extracting the Features 
-        df_new = df[['Title','Cast and crew','genres']]
-        # here creating new director name column to store the splitted data
-        df_new['director_name'] = df_new['Cast and crew'].map(lambda x: get_director(str(x)))
-        # here taking the actors name [1st index values]
-        df_new['actor_1_name'] = df_new['Cast and crew'].map(lambda x: get_actor_one(str(x)))
-        # here taking the actors name [2nd index values]
-        df_new['actor_2_name'] = df_new['Cast and crew'].map(lambda x: get_actor_two(str(x)))
-        # here taking the actors name [2nd index values]
-        df_new['actor_3_name'] = df_new['Cast and crew'].map(lambda x: get_actor_three(str(x)))
-        # here renaming the Title Field Column name to movie_title
-        df_new = df_new.rename(columns={'Title':'movie_title'})
-        # extract the feature that we need to train our model
-        new_df = df_new.loc[:,['director_name','actor_1_name','actor_2_name','actor_3_name','genres','movie_title']]
-        # Replacing the nan values to ('unknown')
-        new_df['actor_2_name'] = new_df['actor_2_name'].replace(np.nan, 'unknown')
-        new_df['actor_3_name'] = new_df['actor_3_name'].replace(np.nan, 'unknown')
-        # lowercasing the movie_title
+        df_new = df[['Title', 'Cast and crew', 'genres']]
+        df_new['director_name'] = df_new['Cast and crew'].map(
+            lambda x: get_director(str(x)))
+        df_new['actor_1_name'] = df_new['Cast and crew'].map(
+            lambda x: get_actor_one(str(x)))
+        df_new['actor_2_name'] = df_new['Cast and crew'].map(
+            lambda x: get_actor_two(str(x)))
+        df_new['actor_3_name'] = df_new['Cast and crew'].map(
+            lambda x: get_actor_three(str(x)))
+        df_new = df_new.rename(columns={'Title': 'movie_title'})
+        new_df = df_new.loc[:, ['director_name', 'actor_1_name',
+                                'actor_2_name', 'actor_3_name', 'genres', 'movie_title']]
+        new_df['actor_2_name'] = new_df['actor_2_name'].replace(
+            np.nan, 'unknown')
+        new_df['actor_3_name'] = new_df['actor_3_name'].replace(
+            np.nan, 'unknown')
         new_df['movie_title'] = new_df['movie_title'].str.lower()
-        # combining data
-        new_df['comb'] = new_df['actor_1_name'] + ' ' + new_df['actor_2_name'] + ' '+ new_df['actor_3_name'] + ' '+ new_df['director_name'] +' ' + new_df['genres']
-        # dropping the null data
-        # If any NA values are present, drop that row or column
+        new_df['comb'] = new_df['actor_1_name'] + ' ' + new_df['actor_2_name'] + ' ' + \
+            new_df['actor_3_name'] + ' ' + \
+            new_df['director_name'] + ' ' + new_df['genres']
         new_df = new_df.dropna(how='any')
-        # Our S3 Updated Data
         updated_data = s3_updated_dataset()
-        print(f"Before Updating : {updated_data.shape}")
         if updated_data is not None:
-            # Appending old Update data with new Dataset Generated 
-            new_data = pd.concat([updated_data,new_df])
-            # If any NA values are present, drop that row or column
+            new_data = pd.concat([updated_data, new_df])
             new_data = new_data.dropna(how='any')
-            # dropping the duplicate values
-            new_data.drop_duplicates(subset ="movie_title", keep = 'last', inplace = True)
-            print('after dropping items')
-            # before update S3 dataset
-            new_data.to_csv(file_path+file_name,index=False) # keep the data in local storage
-            uploaded = Trigger_Uploader(file_path=file_path,file_name=file_name)
+            new_data.drop_duplicates(
+                subset="movie_title", keep='last', inplace=True)
+            new_data.to_csv(file_path+file_name, index=False)
+            uploaded = Trigger_Uploader(
+                file_path=file_path, file_name=file_name)
             if uploaded == "Uploaded to S3 bucket":
-                after_updated_data = s3_updated_dataset()
-                print(f"After Updating : {after_updated_data.shape}")
+                s3_updated_dataset()
         else:
             raise Exception("UpdateData not getting from S3")
     except Exception as e:
         print(e)
 
-def wikipedia_data_scrapper(country,year):
+
+def wikipedia_data_scrapper(country, year):
+    """
+        1. Here Collection Movie Details From Wikipedia based on the year and country that provided by user.
+        2. Merging all the tables that we have extracted from wikipedia
+    """
     link = f"https://en.wikipedia.org/wiki/List_of_{country.capitalize()}_films_of_{year}"
     df1 = pd.read_html(link, header=0)[2]
     df2 = pd.read_html(link, header=0)[3]
     df3 = pd.read_html(link, header=0)[4]
     df4 = pd.read_html(link, header=0)[5]
-    # Merging all the data's
-    df = df1.append(df2.append(df3.append(df4,ignore_index=True),ignore_index=True),ignore_index=True)
-    # df = pd.concat([df1,df2,df3,df4])
+    df = df1.append(df2.append(df3.append(df4, ignore_index=True),
+                    ignore_index=True), ignore_index=True)
     return df
+
 
 try:
     if API_KEY is None:
         raise Exception("API_KEY is not Getting")
-    def make_new_dataset(country,year):
+
+    def make_new_dataset(country, year):
         """
             1. Scrapping Movies Title from Wikipedia
             2. Collecting Genres from TMDB Using API
@@ -140,12 +161,13 @@ try:
         """
         current_year = datetime.today().year
         if current_year != year:
-            df = wikipedia_data_scrapper(country,year)
+            df = wikipedia_data_scrapper(country, year)
             df['genres'] = df['Title'].map(lambda x: get_genre(str(x)))
             preprocess_data(df)
         else:
-            current_month = " ".join(list_of_months[str(datetime.today().month)].upper())
-            df = wikipedia_data_scrapper(country,year)
+            current_month = " ".join(
+                list_of_months[str(datetime.today().month)].upper())
+            df = wikipedia_data_scrapper(country, year)
             df['new_data'] = df['Opening'].map(lambda x: str(x))
             for i in range(len(df['new_data'])):
                 month = df['new_data'][i]
@@ -156,10 +178,9 @@ try:
             preprocess_data(df)
 except Exception as e:
     print(e)
-    
+
 if __name__ == '__main__':
     country = "American"
     year = 2022
-    make_new_dataset(country=country,year=year)
+    make_new_dataset(country=country, year=year)
     print(f"done :{year} dataset updated")
-    
