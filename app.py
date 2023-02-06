7d0741437f9d8c559469b28fd66496681eeddcdc
Thanseef <androtsf@gmail.com>
2023-02-06 13:10:57 +0530

    model loading done, working on api for movie details

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 ###################################### IMPORTING LIBRARIES ############################################
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 import json
 import random
+import requests
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 ###################################### FASTAPI SETTING UP #############################################
 
@@ -17,3 +22,25 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+###################################### LOADING THE MODELS #############################################
+
+file_path = "model/"
+trained_model = "trained_model.pkl"
+transformed_model = "transformed.pkl"
+classifier = pickle.load(open(file_path+trained_model, 'rb'))
+vectorizer = pickle.load(open(file_path+transformed_model,'rb'))
+
+###################################### Working with apis #############################################
+        
+    
+@app.post("/api/movieDetails")
+async def getMovieDetails(movie_details : Request):
+    collected_data = await movie_details.json()
+    movie_title = collected_data['movie_title']
+    return {
+      "status" : status.HTTP_200_OK,
+      "results" : {
+        "move_title" : movie_title
+      }
+    }
